a3821274165ab3ca35bc1343f9a246bc38c0d971
aaronisme <aarondongchen@gmail.com>
2021-04-01 11:42:16 +0800

    add the starter contract

---
+++ contracts/ERC721Tradeable.sol
@@ -0,0 +1,53 @@
+pragma solidity ^0.5.0;
+
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "./Strings.sol";
+
+/**
+ * @title ERC721Tradable
+ * ERC721Tradable - ERC721 contract that whitelists a trading address, and has minting functionality.
+ */
+contract ERC721Tradable is ERC721Full, Ownable {
+    using Strings for string;
+
+    uint256 private _currentTokenId = 0;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+    ) public ERC721Full(_name, _symbol)
+
+    /**
+     * @dev Mints a token to an address with a tokenURI.
+     * @param _to address of the future owner of the token
+     */
+    function mintTo(address _to) public onlyOwner {
+        uint256 newTokenId = _getNextTokenId();
+        _mint(_to, newTokenId);
+        _incrementTokenId();
+    }
+
+    /**
+     * @dev calculates the next token ID based on value of _currentTokenId
+     * @return uint256 for the next token ID
+     */
+    function _getNextTokenId() private view returns (uint256) {
+        return _currentTokenId.add(1);
+    }
+
+    /**
+     * @dev increments the value of _currentTokenId
+     */
+    function _incrementTokenId() private {
+        _currentTokenId++;
+    }
+
+    function baseTokenURI() public pure returns (string memory) {
+        return "";
+    }
+
+    function tokenURI(uint256 _tokenId) external view returns (string memory) {
+        return Strings.strConcat(baseTokenURI(), Strings.uint2str(_tokenId));
+    }
+}
---
+++ contracts/Nft.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.0;
+
+import "./ERC721Tradbeable.sol";
+
+contract MyNFT is ERC721Tradable {
+    constructor() ERC721Tradable("MyNFT", "MNFT")
+}
+
+
+orient crucial bleak lazy illegal canal evil initial ranch sibling interest polar
